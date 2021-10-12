1f8056f0beb50c2d09e39071467ae1feb5b643c9
ameybhosle1 <ameybhosle3@gmail.com>
2021-10-12 20:04:02 +0530

    2nd commit

---
+++ src/context/Provider.js
@@ -0,0 +1,20 @@
+import React , {useState} from "react";
+import PackageContext  from './Context'
+
+const Provider = (props)=>{
+    const [mission , setMission] = useState({
+        mname:'Go To Russia',
+        Agent:007,
+        accept:false
+    })
+    return(
+        <PackageContext.Provider value={{
+            data:mission,
+            isAccepted:()=>{
+                setMission(...mission , accept = true)
+            }
+        }}>
+            {props.children}
+        </PackageContext.Provider>
+    );
+}
