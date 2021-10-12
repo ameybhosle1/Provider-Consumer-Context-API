82e2894187e46eff36e8adb96d01ddb33bf9164a
ameybhosle1 <ameybhosle3@gmail.com>
2021-10-12 20:58:49 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import React , {Fragment} from "react";
+import Provider from './context/Provider';
+import Context from "./context/Context";
+
+function AgentTwo() {
+  return (
+    <Context.Consumer>
+      {
+        (context) => (
+          <Fragment>
+            <h3>AgentInfoo {context.data.mname}</h3>
+          </Fragment>
+        )
+      }
+    </Context.Consumer>
+  );
+}
+
+function AgentOne() {
+  return <AgentTwo />
+}
+
+function Agents() {
+  return (
+    <AgentOne />
+  );
+}
 
 function App() {
   return (
     <div>
-        <h1>FROM REACT</h1>
+        <h1>Context API</h1>
+        <Provider>
+          <Agents />
+        </Provider>
     </div>
   );
 }
